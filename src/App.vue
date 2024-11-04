04dd49d5b65ff53e7d34ddbd4e465942b7b2b3be
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-11-04 14:11:53 +0200

    add for todos

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,185 @@
+<script>
+import todos from './data/todo'
+
+export default {
+  data() {
+    return {
+      todos,
+    }
+  },
+}
+</script>
+
 <template>
   <div class="todoapp">
     <h1 class="todoapp__title">todos</h1>
 
     <div class="todoapp__content">
-      <!-- <Header
-        todos="{todos}"
-        handleQueryChange="{handleQueryChange}"
-        updateCompleted="{updateCompleted}"
-        query="{query}"
-        handleAddTodo="{handleAddTodo}"
-        inputRef="{inputRef}"
-        isLoading="{isLoading}"
-      />
+      <header className="todoapp__header">
+        <button
+          type="button"
+          class="todoapp__toggle-all active"
+          data-cy="ToggleAllButton"
+        ></button>
 
-      <ListComponent
-        todos="{tasks}"
-        handleDelete="{handleDelete}"
-        isLoadingTodos="{isLoadingTodos}"
-        isLoading="{isLoading}"
-        tempTodo="{tempTodo}"
-        updateCompleted="{updateCompleted}"
-        updateTitle="{updateTitle}"
-      />
-      {todos.length > 0 && (
-      <Footer
-        onClearCompleted="{deleteAllCompleted}"
-        setFilterBy="{setFilterBy}"
-        todos="{todos}"
-        filterBy="{filterBy}"
-      />
-      )}-->
+        <form>
+          <input
+            data-cy="NewTodoField"
+            type="text"
+            className="todoapp__new-todo"
+            placeholder="What needs to be done?"
+          />
+        </form>
+      </header>
+
+      <section class="doapp__main" data-cy="TodoList">
+        <div
+          data-cy="Todo"
+          v-for="todo of todos"
+          :key="todo.id"
+          class="todo"
+          :class="{ completed: todo.completed }"
+        >
+          <label class="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              class="todo__status"
+              :checked="todo.completed"
+            />
+          </label>
+
+          <form v-if="false">
+            <input
+              data-cy="TodoTitleField"
+              type="text"
+              class="todo__title-field"
+              placeholder="Empty todo will be deleted"
+              value="Todo is being edited now"
+            />
+          </form>
+          <template v-else>
+            <span data-cy="TodoTitle" class="todo__title">
+              {{ todo.title }}
+            </span>
+
+            <button type="button" class="todo__remove" data-cy="TodoDelete">
+              ×
+            </button>
+          </template>
+
+          <div
+            data-cy="TodoLoader"
+            class="modal overlay"
+            :class="{ 'is-active': false }"
+          >
+            <div clasclasssName="modal-background has-background-white-ter" />
+            <div class="loader" />
+          </div>
+        </div>
+      </section>
+
+      <!-- <div data-cy="Todo" class="todo">
+          <label class="status-label">
+            <input data-cy="TodoStatus" type="checkbox" class="todo__status" />
+          </label>
+
+          <span data-cy="TodoTitle" class="todo__title">
+            Not Completed Todo
+          </span>
+          <button type="button" class="todo__remove" data-cy="TodoDelete">
+            ×
+          </button>
+
+          <div data-cy="TodoLoader" class="modal overlay">
+            <div class="modal-background has-background-white-ter" />
+            <div class="loader" />
+          </div>
+        </div> -->
+
+      <!-- <div data-cy="Todo" class="todo">
+          <label class="todo__status-label">
+            <input data-cy="TodoStatus" type="checkbox" class="todo__status" />
+          </label>
+
+          <form>
+            <input
+              data-cy="TodoTitleField"
+              type="text"
+              class="todo__title-field"
+              placeholder="Empty todo will be deleted"
+              value="Todo is being edited now"
+            />
+          </form>
+
+          <div data-cy="TodoLoader" class="dal overlay">
+            <div class="ground has-background-white-ter" />
+            <div class="ader" />
+          </div>
+        </div>
+
+        <div data-cy="Todo" class="todo">
+          <label class="todo__status-label">
+            <input data-cy="TodoStatus" type="checkbox" class="todo__status" />
+          </label>
+
+          <span data-cy="TodoTitle" class="todo__title">
+            Todo is being saved now
+          </span>
+
+          <button type="button" class="todo__remove" data-cy="TodoDelete">
+            ×
+          </button>
+
+          <div data-cy="TodoLoader" class="modal overlay is-active">
+            <div class="modal-background has-background-white-ter" />
+            <div class="loader" />
+          </div>
+        </div> -->
+
+      <footer class="todoapp__footer" data-cy="Footer">
+        <span class="todo-count" data-cy="TodosCounter"> 3 items left </span>
+
+        <nav class="filter" data-cy="Filter">
+          <a href="#/" class="filter__link selected" data-cy="FilterLinkAll">
+            All
+          </a>
+
+          <a href="#/active" class="filter__link" data-cy="FilterLinkActive">
+            Active
+          </a>
+
+          <a
+            href="#/completed"
+            class="filter__link"
+            data-cy="FilterLinkCompleted"
+          >
+            Completed
+          </a>
+        </nav>
+
+        <button
+          type="button"
+          class="todoapp__clear-completed"
+          data-cy="ClearCompletedButton"
+        >
+          Clear completed
+        </button>
+      </footer>
     </div>
-    <Error errorMessage="{errorMessage}" setErrorMessage="{setErrorMessage}" />
+    <div
+      data-cy="ErrorNotification"
+      class="notification is-danger is-light has-text-weight-normal"
+    >
+      <button data-cy="HideErrorButton" type="button" class="delete" />
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
   </div>
 </template>
